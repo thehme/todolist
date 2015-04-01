67e69271760434142f92e68a3baa3feb0d786359
thehme <bcortez23@gmail.com>
2015-03-31 22:15:14 -0400

    server.js file

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(path.join(__dirname, '/angularjs'))); //  "public" off of current is root
+
+app.listen(8000);
+console.log('Listening on port 8000');
+
